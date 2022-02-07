7d99b5b6b076a35a1d16ae8ac6bda54043808096
dloughborough <david.loughborough@kellogg.northwestern.edu>
2022-02-07 18:45:56 +0000

    added loop for roles

--- kmdb.rb
+++ kmdb.rb
@@ -285,8 +285,8 @@ puts "========"
 puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie
-movies = Movie.all
-for movie in movies
-    role = movie.role
+roles = Role.all
+for role in roles
+    person = role.person
     puts "#{movie.title} #{person.name} #{role.character_name}"
 end
